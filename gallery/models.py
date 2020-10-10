bc8d998c737f97d7ba558dd857a3b4a32e32887a
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-11 00:06:33 +0300

    add comments section and bootstrap with bootstrap forms

---
+++ gallery/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+  image = models.ImageField()
+  descritption = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
+  modified = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.descritption[:50]
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  text = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
+  modified = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.text[:50]
