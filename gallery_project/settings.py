42212dde80697862531530691c30f6ba02aab70e
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-10-13 08:58:01 +0300

    static file

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -147,9 +147,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [(
-    os.path.join(BASE_DIR, 'static')
-)]
+STATICFILES_DIRS = [STATIC_DIR]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
